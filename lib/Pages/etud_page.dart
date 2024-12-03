0a64f696082b94c5c013cd59c624d6847fd2630e
Manal <manal.kerroumi@gmail.com>
2024-12-03 20:05:29 +0100

    Mise en place de l'agenda pour l'étudiant

--- lib/Pages/etud_page.dart
+++ lib/Pages/etud_page.dart
@@ -1,11 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:student_manager/Pages/add_task_page.dart';
 import 'package:student_manager/Pages/view_tasks_page.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class EtudPage extends StatelessWidget {
+class EtudPage extends StatefulWidget {
   final String userId; // Paramètre pour stocker l'ID utilisateur
 
   const EtudPage({Key? key, required this.userId}) : super(key: key);
+
+  @override
+  _EtudPageState createState() => _EtudPageState();
+}
+
+class _EtudPageState extends State<EtudPage> {
+  int _selectedIndex = 0; // Onglet actif
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getBody() {
+    // Retourne le contenu selon l'onglet actif
+    switch (_selectedIndex) {
+      case 1: // Onglet "Agenda"
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                'Mon Agenda',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Expanded(
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2020, 1, 1),
+                  lastDay: DateTime.utc(2030, 12, 31),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  },
+                  calendarStyle: CalendarStyle(
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.teal,
+                      shape: BoxShape.circle,
+                    ),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    weekendTextStyle: const TextStyle(color: Colors.red),
+                  ),
+                  headerStyle: const HeaderStyle(
+                    titleCentered: true,
+                    formatButtonVisible: false,
+                  ),
+                  daysOfWeekStyle: const DaysOfWeekStyle(
+                    weekendStyle: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      case 0: // Onglet "Tâche"
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'Bienvenue dans l’espace Étudiant : ',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Choisissez une action à effectuer.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+
+              // Bouton pour ajouter une tâche
+              _buildActionCard(
+                context,
+                icon: Icons.task,
+                title: 'Ajouter une tâche',
+                description: 'Créer une nouvelle tâche.',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddTaskPage(userId: widget.userId),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        );
+      default: // Par défaut : "Rechercher"
+        return const Center(
+          child: Text(
+            'Rechercher quelque chose ici...',
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +141,15 @@ class EtudPage extends StatelessWidget {
           PopupMenuButton<String>(
             onSelected: (value) {
               if (value == 'tasks') {
-                // Rediriger vers la liste des tâches
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => ViewTasksPage(userId: userId),
+                    builder: (context) => ViewTasksPage(userId: widget.userId),
                   ),
                 );
               } else if (value == 'logout') {
-                // Déconnexion
                 Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  '/',
-                  (route) => false,
-                );
+                    context, '/', (route) => false);
               }
             },
             itemBuilder: (BuildContext context) {
@@ -49,50 +167,35 @@ class EtudPage extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            // Titre principal
-            const Text(
-              'Bienvenue dans l’espace Étudiant : ',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.teal,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Choisissez une action à effectuer.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-
-            // Bouton pour ajouter une tâche
-            _buildActionCard(
-              context,
-              icon: Icons.task,
-              title: 'Ajouter une tâche',
-              description: 'Créer une nouvelle tâche.',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddTaskPage(userId: userId),
-                  ),
-                );
+      body: _getBody(),
+      floatingActionButton: _selectedIndex == 1
+          ? FloatingActionButton(
+              onPressed: () {
+                // Action pour ajouter une tâche à partir de l'agenda
               },
-            ),
-            const SizedBox(height: 20),
-          ],
-        ),
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.add, color: Colors.white),
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Tâche',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Agenda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Rechercher',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.teal,
+        onTap: _onItemTapped,
       ),
     );
   }
@@ -121,15 +224,12 @@ class EtudPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
-            // Icône principale
             CircleAvatar(
               radius: 30,
               backgroundColor: Colors.teal.withOpacity(0.1),
               child: Icon(icon, color: Colors.teal, size: 30),
             ),
             const SizedBox(width: 20),
-
-            // Texte descriptif
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
